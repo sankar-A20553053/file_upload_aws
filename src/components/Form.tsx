91e4f89bedbcf2dd3485d5a52db7201a9ec67d7f
Sankar Sivam <sparamasivam@hawk.iit.edu>
2024-04-06 22:41:47 +0530

    S3 intergrated

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,56 @@
+import "./init.tsx";
 import FileUploadRoundedIcon from "@mui/icons-material/FileUploadRounded";
 import { useRef } from "react";
 import axios from "axios";
+import AWS from "aws-sdk";
 import { nanoid } from "nanoid";
 
 function Form() {
   const image = useRef<HTMLInputElement>();
+
+  const uploadFile = async (file) => {
+    // S3 Bucket Name
+    const S3_BUCKET = "bucketfulloftexts";
+
+    // S3 Region
+    const REGION = "us-east-2";
+
+    // S3 Credentials
+    AWS.config.update({
+      accessKeyId: "sdjajskdjasdlkkajlsdlk",
+      secretAccessKey: "asdhashdjasdjasjkdkjasdjnasddkkasd",
+    });
+    const s3 = new AWS.S3({
+      params: { Bucket: S3_BUCKET },
+      region: REGION,
+    });
+
+    // Files Parameters
+
+    const params = {
+      Bucket: S3_BUCKET,
+      Key: file.name,
+      Body: file,
+    };
+
+    // Uploading file to s3
+
+    const upload = s3
+      .putObject(params)
+      .on("httpUploadProgress", (evt) => {
+        // File uploading progress
+        console.log(
+          "Uploading " + parseInt((evt.loaded * 100) / evt.total) + "%"
+        );
+      })
+      .promise();
+
+    await upload.then((err, data) => {
+      console.log(err);
+      // Fille successfully uploaded
+      // alert("File uploaded successfully.");
+    });
+  };
 
   function onImage() {
     image.current?.click();
@@ -14,17 +60,19 @@ function Form() {
     console.log(typeof e);
     e.preventDefault();
     console.log(e.target[0].value);
-    console.log(e.target[1].value);
+    console.log(e.target[1].files[0].name);
 
     const payload = {
-      name: nanoid(),
+      id: nanoid(),
       text: e.target[0].value,
-      path: e.target[1].value,
+      path: `bucketfulloftexts/${e.target[1].files[0].name}`,
     };
+
+    uploadFile(e.target[1].files[0]);
 
     axios
       .post(
-        "https://8imc2bkds2.execute-api.us-east-2.amazonaws.com/prod",
+        "https://8imc2bkds2.execute-api.us-east-2.amazonaws.com/prod/dyno",
         payload
       )
       .then(function (response) {
